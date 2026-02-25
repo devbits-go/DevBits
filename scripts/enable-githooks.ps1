7e62a924cf79986a11e98d4e5fca1f87ad0a6ef1
Eli Fouts <eligfouts@gmail.com>
2026-02-24 21:05:07 -0500

    Removed github hookss not needed. screw you co-piolot

--- scripts/enable-githooks.ps1
+++
@@ -1,3 +0,0 @@
-Write-Host "Setting Git hooksPath to .githooks for repository at $(Get-Location)"
-git config core.hooksPath .githooks
-Write-Host "Run 'git config --local --get core.hooksPath' to verify."
--- scripts/enable-githooks.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "Setting Git hooksPath to .githooks for repository at $(pwd)"
-git config core.hooksPath .githooks
-echo "Run 'git config --local --get core.hooksPath' to verify."
